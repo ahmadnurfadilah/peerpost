2679320f27db54c1d19656eb3841e75607e80e34
Ahmad Nurfadilah <ahmadnurfadilah22@gmail.com>
2023-06-18 10:47:12 +0700

    add landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,49 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      container: {
+        center: true,
+      },
+      colors: {
+        dark: "#222222",
+        lime: "#DEECA3",
+        primary: {
+          50: "#d9f3ff",
+          100: "#badce7",
+          200: "#9ac2d0",
+          300: "#78a8b8",
+          400: "#5e93a6",
+          500: "#438094",
+          600: "#377183",
+          700: "#285d6d",
+          800: "#1b4a58",
+          900: "#073541",
+        },
+        danger: colors.red,
+        success: colors.green,
+        warning: colors.amber,
+      },
+      animation: {
+        updown: "updown 3s ease-in-out infinite",
+      },
+      keyframes: {
+        updown: {
+          "0%, 100%": { transform: "translateY(-10px)" },
+          "50%": { transform: "translateY(10px)" },
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+],
+};
